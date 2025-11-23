0b481dbc95f55318ab35c20e058d5ba3f0820869
Roman Kryvko <roman.kr43@gmail.com>
2025-11-23 09:38:10 +0100

    Create GitHub profile preview.

---
+++ portfolio/src/lib/GitHubPreview.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  const props = $props();
+  let showGitHubLink = $state(false);
+</script>
+
+<div class="github-profile">
+  <button
+    class="github-btn"
+    type="button"
+    onclick={() => {
+      showGitHubLink = !showGitHubLink;
+    }}
+  >
+    <img class="github-avatar" alt="GitHub avatar." src={props?.avatar_url} />
+  </button>
+  {#if showGitHubLink}
+    <a class="github-link link-hidden" href={props?.html_url}>GitHub: <u>{props?.login}</u></a>
+  {/if}
+</div>
+
+<style>
+  .github-profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 20px;
+  }
+  .github-btn {
+    border-radius: 50%;
+    border-color: transparent;
+    background-color: transparent;
+    cursor: pointer;
+    color: var(--text-color);
+  }
+  .github-avatar {
+    display: inline-block;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background: var(--color-primary);
+    text-align: center;
+    visibility: visible;
+  }
+  .github-link:after {
+    font-family: system-ui;
+    font-size: larger;
+    content: '↗';
+  }
+</style>
--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+  import GitHubPreview from '$lib/GitHubPreview.svelte';
+  let { data } = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<GitHubPreview avatar_url={data.avatar_url} html_url={data.html_url} login={data.login} />
