24bc76ad74ef4ebc835f07140f768feb89f0fcac
Roman Kryvko <roman.kr43@gmail.com>
2025-11-23 09:43:23 +0100

    Flatten directory structure.

---
+++ src/routes/+page.ts
@@ -0,0 +1,9 @@
+export const load = async ({ fetch }: any) => {
+  const res = await fetch('https://api.github.com/users/RomanKryvko');
+  if (res.status != 200) {
+    console.error(res);
+    return;
+  }
+  const json = await res.json();
+  return { avatar_url: json.avatar_url, html_url: json.html_url, login: json.login };
+};
