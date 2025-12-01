364b2605c8a617b42bd99f08c1887fb610cf00d6
Roman Kryvko <roman.kr43@gmail.com>
2025-12-01 18:37:41 +0100

    Create SectionHeader component.

---
+++ src/lib/SectionHeader.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  const { id, children } = $props();
+</script>
+
+<div class="section-header-wrapper">
+  <h1 class="section-header" {id}>{@render children()}</h1>
+</div>
+
+<style>
+  .section-header {
+    overflow: hidden;
+    clip-path: polygon(15% 0, 100% 0, 85% 100%, 0% 100%);
+    background-color: var(--color-primary);
+    padding-left: 2em;
+    padding-right: 2em;
+    color: var(--color-background);
+    max-width: fit-content;
+  }
+
+  .section-header:hover {
+    background-color: var(--color-secondary);
+    transition: 0.2s ease;
+  }
+  .section-header-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+</style>
